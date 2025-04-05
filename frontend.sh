b18cc91baf2aa3876aefb2371842144eae144a86
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-05 08:37:53 +0530

    expense

--- frontend.sh
+++ frontend.sh
@@ -54,6 +54,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restart Frontend"
